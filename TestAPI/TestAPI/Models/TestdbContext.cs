85ec5f409d1df85c950d8967893eb2c497d5f150
mnagadiya <mitesh.nagadiya@repariodata.com>
2023-01-11 00:25:19 +0530

    DBContext added

---
+++ TestAPI/TestAPI/Models/TestdbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestAPI.Models;
+
+public partial class TestdbContext : DbContext
+{
+    public TestdbContext()
+    {
+    }
+
+    public TestdbContext(DbContextOptions<TestdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Testtbl> Testtbls { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=IND-RDLAP02; Database=Testdb;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Testtbl>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Testtbl");
+
+            entity.Property(e => e.City)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("ID");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TestAPI/TestAPI/Models/Testtbl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestAPI.Models;
+
+public partial class Testtbl
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public string? Country { get; set; }
+
+    public long? ContactNumber { get; set; }
+}
